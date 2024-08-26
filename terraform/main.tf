222391108ed0280f72e2a987e947536ff189e235
owen.zanzal <ozanzal@gmail.com>
2024-08-26 11:15:42 -0400

    Add CI workflow, Taskfile, and Terraform backend configuration
    - Modified CI workflow (`ci.yml`) to include testing and building tasks using the new Taskfile.
    - Enhanced Terraform workflow (`terraform.yml`) by adding AWS credentials configuration and selecting/creating a Terraform workspace.
    - Updated `.gitignore` to exclude `.env.secret` files.
    - Introduced a new `Taskfile.yml` to define build, test, deploy, and code tasks.
    - Added a binary script (`bin/task`) for task execution.
    - Configured Terraform to use an S3 backend for state management in `main.tf`.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "o3cloud-tf-state"
+    key="terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
